634d82c0bbe6e54dfa7293b116c0494f0cfc21b6
Saksham Wane <37554951+Sakshamwane@users.noreply.github.com>
2025-11-08 19:23:27 +0530

    Time: 0 ms (100%), Space: 29.6 MB (36.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
+    void swap(vector<int> &nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
     void rotate(vector<int>& nums, int k) {
-        //Size of input array
         int n = nums.size();
-        
-        //Edge case
-        if(n == 0) return;
-
-        //Required num of rotations
-        int d = k % n;
-
-        //Edge case
-        if(d == 0) return;
-
-        //Temp array to store elements to be shifted front
-        vector<int> temp(d);
-
-        //Storing temp values to rotate
-        for(int i = 0; i < d; i++){
-            temp[i] = nums[n-d+i];
+        k = k % n;
+        int left = 0; 
+        int right = n-1;
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
         }
-
-        //Shifting remaining elements
-        for(int i = n - d - 1; i >= 0; i--){
-            nums[i+d] = nums[i];
+        left = 0;
+        right = k-1;
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
         }
-
-        //Replaceing rotated values
-        for(int i = 0; i < d; i++){
-            nums[i] = temp[i];
+        left = k;
+        right = n-1;
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
         }
     }
 };
