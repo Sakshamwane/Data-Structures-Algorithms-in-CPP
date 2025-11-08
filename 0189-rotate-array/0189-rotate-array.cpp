8d6a8f5c0058a51f14da25e5bf27ee8fffa08131
Saksham Wane <37554951+Sakshamwane@users.noreply.github.com>
2025-11-08 16:57:57 +0530

    Time: 3 ms (9.5%), Space: 30.1 MB (30.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k){
+    void rotate(vector<int>& nums, int k) {
+        //Size of input array
         int n = nums.size();
+        
+        //Edge case
         if(n == 0) return;
-        int d = k%n;
+
+        //Required num of rotations
+        int d = k % n;
+
+        //Edge case
         if(d == 0) return;
+
+        //Temp array to store elements to be shifted front
         vector<int> temp(d);
+
+        //Storing temp values to rotate
         for(int i = 0; i < d; i++){
-            temp[i] = nums[n - d + i];;
+            temp[i] = nums[n-d+i];
         }
+
+        //Shifting remaining elements
         for(int i = n - d - 1; i >= 0; i--){
             nums[i+d] = nums[i];
-
         }
+
+        //Replaceing rotated values
         for(int i = 0; i < d; i++){
             nums[i] = temp[i];
         }
