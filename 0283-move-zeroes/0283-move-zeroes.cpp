d6957ab091a5ec141946436bfad40bf9c46c0c9d
Saksham Wane <37554951+Sakshamwane@users.noreply.github.com>
2025-11-09 17:26:19 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-
+    //Swap function
     void swap(vector<int> &nums, int a, int b){
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
     }
     void moveZeroes(vector<int>& nums) {
+        //Size of given array
         int n = nums.size();
-
-        //Pointer
+        
+        //Left pointer to position
         int left = 0;
 
-        for(int right = 0; right<n; right++){
+        //Swapping zero with non zero elements
+        for(int right = 0; right < n; right++){
             if(nums[right] != 0){
                 swap(nums, left, right);
                 left++;
