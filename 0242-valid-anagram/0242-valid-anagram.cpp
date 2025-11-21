921456d7fc477d75337bcd6cc996f8157c72cddb
Saksham Wane <37554951+Sakshamwane@users.noreply.github.com>
2025-11-22 00:17:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //Sort both strings
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        //Return true if equal, false if not equal
-        return s == t;
+        //If sizes are not equal return 
+        if(s.size() != t.size()) return false;
+
+        //Array to keep count of each char
+        vector<int> count(26, 0);
+
+        //Count++ for each char in s
+        for(char c : s) count[c - 'a']++;
+
+        //Count-- for each char in t
+        for(char c : t) count[c - 'a']--;
+
+        //If any char count > 0 return false
+        for(int i : count){
+            if(i != 0) return false;
+        }
+        //Otherwise return true
+        return true;
     }
 };
