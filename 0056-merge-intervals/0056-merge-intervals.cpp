f9da6ee100f2ff2f8f2ad2e29b8d05fd039e337e
Saksham Wane <37554951+Sakshamwane@users.noreply.github.com>
2025-12-25 19:12:32 +0530

    Time: 2 ms (92.92%), Space: 23.9 MB (33.84%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //Sort the intervals based on starting time
+        sort(intervals.begin(), intervals.end());
+
+        //Resutlt array to store merged interval
+        vector<vector<int>> ans;
+
+        //Loop through each interval
+        int n = intervals.size();
+        for(int i = 0; i < n; ){
+            //Start of the current merged interval
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            //Merge with all overlapping intervals
+            int j = i+1;
+            while(j < n && intervals[j][0] <= end){
+                end = max(end, intervals[j][1]);
+                j++;
+            }
+
+            //Add the merged interval to result
+            ans.push_back({start, end});
+
+            //Move to next overlapping interval
+            i = j;
+        }
+        return ans;
+    }
+};
